8fa13476fa0615b81460ee61491fcd37f7f86893
vgorthi <gorthivani2020@gmail.com>
2023-09-21 13:16:50 +0530

    scripted pipline node

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,7 +1,7 @@
 def call(){
     node('workstation'){
         sh "find .| sed -e '1d'|xargs rm -rf  "
-        git branch:'main', url:"https://github.com/Gorthivani/${component}"
+        git "${BRANCH_NAME}":'main', url:"https://github.com/Gorthivani/${component}"
         stage('Compile code') {
             common.compile()
 
