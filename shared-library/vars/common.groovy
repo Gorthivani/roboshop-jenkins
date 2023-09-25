144405b2d200d1b984c3da8620ac0c9f36956252
vgorthi <gorthivani2020@gmail.com>
2023-09-25 15:50:34 +0530

    nexus to upload artifact

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -48,7 +48,7 @@ def release() {
     stage('Release') {
         env.nexususer = sh(script: 'aws ssm get-parameter --name "nexusqube.user" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
         env.nexuspass = sh(script: 'aws ssm get-parameter --name "nexusqube.pass" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-        wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: sonarpass]]]) {
+        wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
             if (env.codeType = "nodejs") {
                 sh 'zip -r ${component}.zip server.js node_modules'
             }
